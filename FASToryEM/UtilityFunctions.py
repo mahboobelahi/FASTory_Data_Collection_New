c05a71fa4bb591d2651b7b9ad3ebc2949171ee5a
Mahboob Elahi <46259412+mahboobelahi@users.noreply.github.com>
2022-05-04 11:17:46 +0300

    SQL to CSV function added

--- FASToryEM/UtilityFunctions.py
+++ FASToryEM/UtilityFunctions.py
@@ -1,10 +1,50 @@
-import threading,time,requests,csv,json
+import threading,time,requests,json,socket,csv
 from FASToryEM import Workstation as WkS
 from FASToryEM import configurations as CONFIG
-from FASToryEM.dbModels import EnergyMeasurements,WorkstationInfo
+from FASToryEM.dbModels import EnergyMeasurements, WorkstationInfo,MeasurementsForDemo
 from FASToryEM import db
-from datetime import datetime
-from flask import jsonify
+# import tensorflow  as tf
+# import numpy as np
+
+
+def SQL_queryToCsv(fileName=None, query=None):
+    try:
+        
+        with open(fileName,'w', newline='') as csvFile:
+            
+            csvWriter = csv.writer(csvFile, delimiter=',')
+            csvWriter.writerow(
+                [
+                    "WorkCellID", "RmsVoltage(V)", "RmsCurrent(A)", "Power(W)", "NominalPower",
+                    "%BeltTension", "ActiveZones", "LoadCombination", "Load"
+                ]
+            )
+            for record in query:
+                csvWriter.writerow([
+                    record.WorkCellID, record.RmsVoltage, record.RmsCurrent, record.Power,
+                    record.Nominal_Power, record.BeltTension, record.ActiveZones, record.LoadCombination, record.Load
+                ])
+
+            # send_file("./forWorksation10_PR.csv",
+            #             mimetype= 'text/csv',
+            #             attachment_filename= 'EM_PatternRecognizer.csv',
+            #             as_attachment=True
+            #)
+    except IOError as e:
+         print ("[X-SQL] :",e)
+    # if not query:
+    #     raise ValueError('No data available')
+
+
+
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
 
 #creating data base modles
 def createModels():
@@ -68,14 +108,17 @@ def cnv_cmd(cmd,section,url,url_self):
 def Workstations():
     
     for id in range(1,len(CONFIG.WorkStations)+1):
-        if id !=10:
+        if id !=10 and id!=2:# and id!=1:
             continue
         temp_obj = WkS.Workstation(id,CONFIG.wrkCellLocIP,
-                                    CONFIG.wrkCellLocPort+id)
+                                    CONFIG.make[id-1],CONFIG.type[id-1],
+                                    CONFIG.wrkCellLocPort+id,
+                                    CONFIG.num_Fast,CONFIG.num)
         #temp_obj.WkSINFO()
         #deleting past subscription to EM service.
-        # temp_obj.invoke_EM_service()
+        #temp_obj.invoke_EM_service()
         #now invoke EM service for accurate results
+        temp_obj.get_access_token()
         # send_measurements=threading.Timer(8,temp_obj.invoke_EM_service,args=("start",))
         # send_measurements.daemon=True
         # send_measurements.start()
@@ -83,12 +126,12 @@ def Workstations():
         threading.Thread(target=temp_obj.runApp,daemon=True).start()
         
         #wait a while for server initialization
-        time.sleep(1)
+        #time.sleep(1)
         #check device registration or register device to ZDMP-DAQ component 
         #temp_obj.register_device()
 
         #subscribe device for ASYNC data access
-        temp_obj.sub_or_Unsubscribe_DataSource(True)
+        #temp_obj.sub_or_Unsubscribe_DataSource(True)
 
         #Db functions
         #if you delete DB Schema then call this method. After that comment it.
@@ -96,45 +139,12 @@ def Workstations():
         #uncomment following line when base IP got changed
         temp_obj.updateIP()
 
+#FASTory BT class-prediction function
 
+# def predict(power,load):
+#     model = tf.keras.models.load_model('M_iter3_1.h5', compile=True)
+#     features = np.array(np.append([power],[load]), ndmin=2)
+#     pred = np.argmax(model.predict(features), axis=1) 
+#     #print(f"[X-UTF] {pred[0]}")
+#     return pred[0]   
 
-
-
-
-def dumyMeasurements():
-    with open(CONFIG.FILE_NAME, 'r') as file: 
-        reader = csv.DictReader(file)
-        count=0
-        for row in reader:
-
-            Voltage= row["RMS Voltage (V)"]
-            Current= row["RMS Current (A)"]
-            _Power= row["Power (W)"]
-            Nominal_Power = row["Normalized_Power"]
-            Active_Zones = row["Load Combinations"]#will come from zone status service
-            _Load = row["Load"]
-            json_str=json.dumps((Voltage,Current,_Power,
-            Nominal_Power,Active_Zones,_Load))
-            print(json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2002/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2003/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2004/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2005/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2006/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2009/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2010/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2011/simulate_measurements",json=json_str)
-            time.sleep(.1)
-            requests.post("http://130.230.190.118:2012/simulate_measurements",json=json_str)
-            time.sleep(1)
-             
-
-# from FASToryEM.UtilityFunctions import dumysimulate_Measurements as DM
